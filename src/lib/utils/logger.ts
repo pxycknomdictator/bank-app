5fbff95fab51cbfe4cd6bef1f1d185f10d6dac49
Noman <pxycknomdictator@outlook.com>
2025-08-16 18:22:11 +0500

    add default export logger

--- src/lib/utils/logger.ts
+++ src/lib/utils/logger.ts
@@ -6,8 +6,10 @@ const { combine, colorize, timestamp, printf } = winston.format;
 const formatter = printf(({ level, message, timestamp }) => `${timestamp} [${level}]: ${message}`);
 const formateOptions = combine(colorize(), timestamp(), formatter);
 
-export const logger = winston.createLogger({
+const logger = winston.createLogger({
 	level: env.NODE_ENV === "development" ? "debug" : "info",
 	format: formateOptions,
 	transports: [new winston.transports.Console({ format: formateOptions })]
 });
+
+export default logger;
